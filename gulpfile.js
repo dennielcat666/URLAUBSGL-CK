402559f30640f3f2300c016ca514f6d5731d12c7
Denniel Cat <deny.cat666@yandex.ru>
2020-06-26 17:30:40 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const babel = require('gulp-babel');
 
 sass.compiler = require('node-sass');
 
-gulp.task('dist',  function dev (cb) {
+gulp.task('dist', function dist (cb) {
 
     gulp.src('./src/*.html')
         .pipe(gulp.dest('./dist/'));
